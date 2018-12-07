9630b1d494c5d7af0c73f19f81d4e0dacbfd10e9
Kacper Zieliński <kacper.zielinski@wat.edu.pl>
2018-12-07 22:38:05 +0100

    Dodano wartość do Tabeli MembershipType

--- Vidly_Kurs/Migrations/20181207140257_PopulateMembershipClass.cs
+++ Vidly_Kurs/Migrations/20181207140257_PopulateMembershipClass.cs
@@ -7,6 +7,7 @@ namespace Vidly_Kurs.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql("INSERT INTO MembershipType (Id,SignUpFee,DurationInMonths,DiscountRate) VALUES (1, 0, 0, 0)");
             migrationBuilder.Sql("INSERT INTO MembershipType (Id,SignUpFee,DurationInMonths,DiscountRate) VALUES (2, 30, 1, 10)");
             migrationBuilder.Sql("INSERT INTO MembershipType (Id,SignUpFee,DurationInMonths,DiscountRate) VALUES (4, 300, 12, 20)");
             migrationBuilder.Sql("INSERT INTO MembershipType (Id,SignUpFee,DurationInMonths,DiscountRate) VALUES (3, 90, 3, 15)");
