899f18ef705943b5d41ddf8a129ce3fe636b9f82
Kacper Zieliński <kacper.zielinski@wat.edu.pl>
2018-12-08 12:36:06 +0100

    Szybka poprawka

--- Vidly_Kurs/Startup.cs
+++ Vidly_Kurs/Startup.cs
@@ -37,7 +37,6 @@ namespace Vidly_Kurs
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<Vidly_KursContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Vidly_KursContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
