e860e78c5696ef00277b7c533aab43b84bb31ba4
Kacper Zieliński <kacper.zielinski@wat.edu.pl>
2018-12-08 13:41:44 +0100

    Rozwiązanie zadania S3EX2

---
+++ Vidly_Kurs/Migrations/20181208121627_AddCustomerBirthdayDate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly_Kurs.Migrations
+{
+    public partial class AddCustomerBirthdayDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthdayDate",
+                table: "Customers",
+                nullable: true
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthdayDate",
+                table: "Customers");
+        }
+    }
+}
---
+++ Vidly_Kurs/Migrations/20181208122037_CustomerBirthdayDateUpdateValues.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vidly_Kurs.Migrations
+{
+    public partial class CustomerBirthdayDateUpdateValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn:"Id",
+                keyValue:"1",
+                column:"BirthdayDate",
+                value: "06/01/1990" 
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
